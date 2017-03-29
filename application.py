a099770e63ea4cf82cd07c6dd39eabea62b3de7f
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-03-29 07:50:41 +0100

    adding messenger

--- application.py
+++ application.py
@@ -39,7 +39,7 @@ def reply(user_id, msg):
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
-@app.route('/webhook', methods=['POST'])
+@application.route('/webhook', methods=['POST'])
 def handle_incoming_messages():
     data = request.json
     sender = data['entry'][0]['messaging'][0]['sender']['id']
