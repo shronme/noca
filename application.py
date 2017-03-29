6df4bfef7dbd1ac96eeb2da0839d3e0ab5c40a0f
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-03-29 07:39:49 +0100

    adding messenger

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import requests
+
 
 # print a nice greeting.
 def say_hello(username = "World"):
@@ -25,6 +27,26 @@ application.add_url_rule('/', 'index', (lambda: header_text +
 # # URL.
 # application.add_url_rule('/<username>', 'hello', (lambda username:
 #     header_text + say_hello(username) + home_link + footer_text))
+
+ACCESS_TOKEN = "EAAYquG24IwkBAPigmNaZC7zKpnbmrjcxLinW9FZCKqmTkuuoN3Jaddb505v9FCWZAZBc5exqutYRKRWZBzoSmsohrpQ0K0qV621GTk4FGaISlky6qN9Gjx07sXXHVvg5HaVkj0ZBbEyyjnbmlZAILoc3kXR5HjV0XfUjabVyhtnAAZDZD"
+
+
+def reply(user_id, msg):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    print(resp.content)
+
+@app.route('/webhook', methods=['POST'])
+def handle_incoming_messages():
+    data = request.json
+    sender = data['entry'][0]['messaging'][0]['sender']['id']
+    message = data['entry'][0]['messaging'][0]['message']['text']
+    reply(sender, message[::-1])
+
+    return "ok"
 
 @application.route('/webhook', methods=['GET'])
 def handle_verification():
