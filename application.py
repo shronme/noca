abf8476436994bb9c34a71af774e2b4f1bff150e
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-03-29 19:38:49 +0100

    adding error handling

--- application.py
+++ application.py
@@ -43,8 +43,17 @@ def reply(user_id, msg):
 def handle_incoming_messages():
     data = request.json
     sender = data['entry'][0]['messaging'][0]['sender']['id']
-    message = data['entry'][0]['messaging'][0]['message']['text']
-    reply(sender, message[::-1])
+    print('the message is: ', data['entry'][0]['messaging'][0]['message'])
+    try:
+        message = data['entry'][0]['messaging'][0]['message']['text']
+    except KeyError:
+        reply(sender, 'oops, something went wrong')
+        return 'ok'
+    try:
+        reply(sender, 'Your message backwards is {}'.format(message[::-1]))
+    except UnicodeEncodeError:
+        reply(sender, 'oops, something went wrong')
+        return 'ok'
 
     return "ok"
 
