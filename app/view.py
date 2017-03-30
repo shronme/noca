885563e2b20df66a199bc13ec6605afa1d2323fd
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-03-30 20:58:10 +0100

    trying buttons

--- app/view.py
+++ app/view.py
@@ -35,8 +35,46 @@ class WebhookView(MethodView):
 	def reply(self, user_id, msg):
 	    data = {
 	        "recipient": {"id": user_id},
-	        "message": {"text": msg}
-	    }
+	        'message': {
+		        'attachment': {
+		        	'type': "template",
+		        	'payload': {
+		        		'template_type': "generic",
+		        		'elements': [{
+			        		'title': "rift",
+			        		'subtitle': "Next-generation virtual reality",
+			        		'item_url': "https://www.oculus.com/en-us/rift/",
+			        		'image_url': "http://messengerdemo.parseapp.com/img/rift.png",
+			        		'buttons': [{
+			        			'type': "web_url",
+			        			'url': "https://www.oculus.com/en-us/rift/",
+			        			'title': "Open Web URL"
+		        			}, 
+		        			{
+		        				'type': "postback",
+		        				'title': "Call Postback",
+		        				'payload': "Payload for first bubble",
+	    				}],
+						}, 
+						{
+							'title': "touch",
+							'subtitle': "Your Hands, Now in VR",
+							'item_url': "https://www.oculus.com/en-us/touch/",
+							'image_url': "http://messengerdemo.parseapp.com/img/touch.png",
+							'buttons': [{
+								'type': "web_url",
+								'url': "https://www.oculus.com/en-us/touch/",
+								'title': "Open Web URL"
+							}, 
+							{
+								'type': "postback",
+								'title': "Call Postback",
+								'payload': "Payload for second bubble",
+							}]
+						}]
+					}
+				}
+		    }
 	    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
 	    print(resp.content)
 
