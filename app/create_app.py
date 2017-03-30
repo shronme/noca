eb86856ead2ac998e48f549e71b5de21ca8c69ac
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-03-30 20:34:55 +0100

    fixing app

--- app/create_app.py
+++ app/create_app.py
@@ -4,39 +4,6 @@ import os
 from view import WebhookView
 
 
-ACCESS_TOKEN = "EAAYquG24IwkBAPigmNaZC7zKpnbmrjcxLinW9FZCKqmTkuuoN3Jaddb505v9FCWZAZBc5exqutYRKRWZBzoSmsohrpQ0K0qV621GTk4FGaISlky6qN9Gjx07sXXHVvg5HaVkj0ZBbEyyjnbmlZAILoc3kXR5HjV0XfUjabVyhtnAAZDZD"
-
-
-def reply(user_id, msg):
-    data = {
-        "recipient": {"id": user_id},
-        "message": {"text": msg}
-    }
-    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-    print(resp.content)
-
-
-def handle_incoming_messages():
-    data = request.json
-    sender = data['entry'][0]['messaging'][0]['sender']['id']
-    print('the message is: ', data['entry'][0]['messaging'][0]['message'])
-    try:
-        message = data['entry'][0]['messaging'][0]['message']['text']
-
-    except KeyError:
-        reply(sender, 'oops, something went wrong')
-        return 'ok'
-    try:
-        reply(sender, 'Your message backwards is {}'.format(message[::-1]))
-    except UnicodeEncodeError:
-        reply(sender, 'oops, something went wrong')
-        return 'ok'
-
-    return "ok"
-
-def handle_verification():
-    return request.args['hub.challenge']
-
 def create_app():
     header_text = '''
     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
--- app/view.py
+++ app/view.py
@@ -10,15 +10,49 @@ class WebhookView(MethodView):
 		    message = data['entry'][0]['messaging'][0]['message']['text']
 
 		except KeyError:
-		    reply(sender, 'oops, something went wrong')
+		    self.reply(sender, 'oops, something went wrong')
 		    return 'ok'
 		try:
-		    reply(sender, 'Your message backwards is {}'.format(message[::-1]))
+		    self.reply(sender, 'Your message backwards is {}'.format(message[::-1]))
 		except UnicodeEncodeError:
-		    reply(sender, 'oops, something went wrong')
+		    self.reply(sender, 'oops, something went wrong')
 		    return 'ok'
 
 		return 'ok'
 
 	def get(self):
 		return request.args['hub.challenge']
+
+
+	ACCESS_TOKEN = "EAAYquG24IwkBAPigmNaZC7zKpnbmrjcxLinW9FZCKqmTkuuoN3Jaddb505v9FCWZAZBc5exqutYRKRWZBzoSmsohrpQ0K0qV621GTk4FGaISlky6qN9Gjx07sXXHVvg5HaVkj0ZBbEyyjnbmlZAILoc3kXR5HjV0XfUjabVyhtnAAZDZD"
+
+
+	def reply(self, user_id, msg):
+	    data = {
+	        "recipient": {"id": user_id},
+	        "message": {"text": msg}
+	    }
+	    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+	    print(resp.content)
+
+
+	# def handle_incoming_messages(self):
+	#     data = request.json
+	#     sender = data['entry'][0]['messaging'][0]['sender']['id']
+	#     print('the message is: ', data['entry'][0]['messaging'][0]['message'])
+	#     try:
+	#         message = data['entry'][0]['messaging'][0]['message']['text']
+
+	#     except KeyError:
+	#         reply(sender, 'oops, something went wrong')
+	#         return 'ok'
+	#     try:
+	#         reply(sender, 'Your message backwards is {}'.format(message[::-1]))
+	#     except UnicodeEncodeError:
+	#         reply(sender, 'oops, something went wrong')
+	#         return 'ok'
+
+	#     return "ok"
+
+	def handle_verification(self):
+	    return request.args['hub.challenge']
