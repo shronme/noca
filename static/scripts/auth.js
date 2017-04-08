24b5db2175b24435eb945ed33a6407a36dc293f1
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-04-08 23:26:29 +0100

    fixing video

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -62,7 +62,7 @@ function noStream(e) {
   if (e.code == 1) {
     msg = 'User denied access to use camera.';
   }
-  document.getElementById('errorMessage').textContent = msg;
+  //document.getElementById('errorMessage').textContent = msg;
 }
 
 function capture() {
