fc5ed6866635cc050e117fa451f1727eed15aeef
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-04-14 22:22:50 +0100

    trying new auth

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -1,120 +1,163 @@
-navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia;
-window.URL = window.URL || window.webkitURL;
+// References to all the element we will need.
+var video = document.querySelector('#camera-stream'),
+    image = document.querySelector('#snap'),
+    start_camera = document.querySelector('#start-camera'),
+    controls = document.querySelector('.controls'),
+    take_photo_btn = document.querySelector('#take-photo'),
+    delete_photo_btn = document.querySelector('#delete-photo'),
+    download_photo_btn = document.querySelector('#download-photo'),
+    error_message = document.querySelector('#error-message');
 
-var app = document.getElementById('app');
-var video = document.getElementById('monitor');
-var snapshot = document.getElementById('snapshot');
-var canvas = document.getElementById('photo');
-var effect = document.getElementById('effect');
-var gallery = document.getElementById('gallery');
-var ctx = canvas.getContext('2d');
-var intervalId = null;
-var idx = 0;
-var filters = [
-  'grayscale',
-  'sepia',
-  'blur',
-  'brightness',
-  'contrast',
-  'hue-rotate', 'hue-rotate2', 'hue-rotate3',
-  'saturate',
-  'invert',
-  ''
-];
 
-function changeFilter(el) {
-  el.className = '';
-  var effect = filters[idx++ % filters.length];
-  if (effect) {
-    el.classList.add(effect);
+// The getUserMedia interface is used for handling camera input.
+// Some browsers need a prefix so here we're covering all the options
+navigator.getMedia = ( navigator.getUserMedia ||
+                      navigator.webkitGetUserMedia ||
+                      navigator.mozGetUserMedia ||
+                      navigator.msGetUserMedia);
+
+
+if(!navigator.getMedia){
+  displayErrorMessage("Your browser doesn't have support for the navigator.getUserMedia interface.");
+}
+else{
+
+  // Request the camera.
+  navigator.getMedia(
+    {
+      video: true
+    },
+    // Success Callback
+    function(stream){
+
+      // Create an object URL for the video stream and
+      // set it as src of our HTLM video element.
+      video.src = window.URL.createObjectURL(stream);
+
+      // Play the video element to start the stream.
+      video.play();
+      video.onplay = function() {
+        showVideo();
+      };
+
+    },
+    // Error Callback
+    function(err){
+      displayErrorMessage("There was an error with accessing the camera stream: " + err.name, err);
+    }
+  );
+
+}
+
+
+
+// Mobile browsers cannot play video without user input,
+// so here we're using a button to start it manually.
+start_camera.addEventListener("click", function(e){
+
+  e.preventDefault();
+
+  // Start video playback manually.
+  video.play();
+  showVideo();
+
+});
+
+
+take_photo_btn.addEventListener("click", function(e){
+
+  e.preventDefault();
+
+  var snap = takeSnapshot();
+
+  // Show image. 
+  image.setAttribute('src', snap);
+  image.classList.add("visible");
+
+  // Enable delete and save buttons
+  delete_photo_btn.classList.remove("disabled");
+  download_photo_btn.classList.remove("disabled");
+
+  // Set the href attribute of the download button to the snap url.
+  download_photo_btn.href = snap;
+
+  // Pause video playback of stream.
+  video.pause();
+
+});
+
+
+delete_photo_btn.addEventListener("click", function(e){
+
+  e.preventDefault();
+
+  // Hide image.
+  image.setAttribute('src', "");
+  image.classList.remove("visible");
+
+  // Disable delete and save buttons
+  delete_photo_btn.classList.add("disabled");
+  download_photo_btn.classList.add("disabled");
+
+  // Resume playback of stream.
+  video.play();
+
+});
+
+
+
+function showVideo(){
+  // Display the video stream and the controls.
+
+  hideUI();
+  video.classList.add("visible");
+  controls.classList.add("visible");
+}
+
+
+function takeSnapshot(){
+  // Here we're using a trick that involves a hidden canvas element.  
+
+  var hidden_canvas = document.querySelector('canvas'),
+      context = hidden_canvas.getContext('2d');
+
+  var width = video.videoWidth,
+      height = video.videoHeight;
+
+  if (width && height) {
+
+    // Setup a canvas with the same dimensions as the video.
+    hidden_canvas.width = width;
+    hidden_canvas.height = height;
+
+    // Make a copy of the current frame in the video on the canvas.
+    context.drawImage(video, 0, 0, width, height);
+
+    // Turn the canvas image into a dataURL that can be used as a src for our photo.
+    return hidden_canvas.toDataURL('image/png');
   }
 }
 
-function gotStream(stream) {
-  if (window.URL) {
-    video.src = window.URL.createObjectURL(stream);
 
-  } else {
-    video.src = stream; // Opera.
+function displayErrorMessage(error_msg, error){
+  error = error || "";
+  if(error){
+    console.log(error);
   }
-  snapshot.src = ''
-  video.onerror = function(e) {
-    stream.stop();
-  };
 
-  stream.onended = noStream;
+  error_message.innerText = error_msg;
 
-  video.onloadedmetadata = function(e) { // Not firing in Chrome. See crbug.com/110938.
-    document.getElementById('splash').hidden = true;
-    document.getElementById('app').hidden = false;
-  };
-
-  // Since video.onloadedmetadata isn't firing for getUserMedia video, we have
-  // to fake it.
-  setTimeout(function() {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    document.getElementById('splash').hidden = true;
-    document.getElementById('app').hidden = false;
-  }, 50);
+  hideUI();
+  error_message.classList.add("visible");
 }
 
-function noStream(e) {
-  var msg = 'No camera available.';
-  if (e.code == 1) {
-    msg = 'User denied access to use camera.';
-  }
-  //document.getElementById('errorMessage').textContent = msg;
+
+function hideUI(){
+  // Helper function for clearing the app UI.
+
+  controls.classList.remove("visible");
+  start_camera.classList.remove("visible");
+  video.classList.remove("visible");
+  snap.classList.remove("visible");
+  error_message.classList.remove("visible");
 }
-
-function capture() {
-  if (intervalId) {
-    clearInterval(intervalId);
-    intervalId = null;
-    return;
-  }
-
-  intervalId = setInterval(function() {
-    ctx.drawImage(video, 0, 0);
-    var img = document.createElement('img');
-    img.src = canvas.toDataURL('image/png');
-    snapshot.src = img.src
-    var angle = Math.floor(Math.random() * 36);
-    var sign = Math.floor(Math.random() * 2) ? 1 : -1;
-    img.style.webkitTransform = 'rotateZ(' + (sign * angle) + 'deg)';
-
-    var maxLeft = document.body.clientWidth;
-    var maxTop = document.body.clientHeight;
-
-    img.style.top = Math.floor(Math.random() * maxTop) + 'px';
-    img.style.left = Math.floor(Math.random() * maxLeft) + 'px';
-
-    gallery.appendChild(img);
-  }, 150);
-}
-
-function init(el) {
-  if (!navigator.getUserMedia) {
-    document.getElementById('errorMessage').innerHTML = 'Sorry. <code>navigator.getUserMedia()</code> is not available.';
-    return;
-  }
-  el.onclick = capture;
-  el.textContent = 'Snapshot';
-  navigator.getUserMedia({video: true}, gotStream, noStream);
-}
-
-window.addEventListener('keydown', function(e) {
-  if (e.keyCode == 27) { // ESC
-    document.querySelector('details').open = false;
-  }
-}, false);
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-22014378-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
