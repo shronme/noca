11c72c5812130826501ee54eef9d1828bcde7b13
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-04-20 21:06:03 +0100

    fixing greetings and geting started

--- app/view.py
+++ app/view.py
@@ -49,9 +49,11 @@ class WebhookView(MethodView):
 			if message == 'GET_HELP':
 				print('in get help')
 				reply(sender, 'Please tell us how can we help and one of our agents will respond promptly')
+				return 'ok'
 			elif message == 'GET_STARTED':
 				reply(sender, 
 					'Hi and welcome to Noca Pay.\nAs a new customer your first purchase, up to £10 is on us. After that we will ask you to register in order to make further purchases.')
+				return 'ok'
 				#TODO Stop bot for this thread until issue resolved
 		else:
 			reply(sender, 'oops, something went wrong')
