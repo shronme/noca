5f526c43a647bd8e11b29c978fc748cae92806d8
Tali Shteinberg <talishteinberg@Talis-MacBook-Air.local>
2017-04-23 13:39:32 +0100

    fixing rek

--- app/view.py
+++ app/view.py
@@ -56,39 +56,53 @@ class WebhookView(MethodView):
 					rek = boto3.client('rekognition', region_name='eu-west-1')
 					s3.Bucket('noca-auth-library').put_object(Key=image_name, Body=image)
 					target_obj = s3.Object('noca-auth-library',image_name).wait_until_exists()
-					source_image_name = '{name}/source/{name}-source.jpeg'.format(name=user.name)
-					print('source name: ', source_image_name)
-					print('target name: ', image_name)
-					
-					response = rek.compare_faces(
-						SimilarityThreshold=0,
-						SourceImage={
+					face_detect = rek.detect_faces(
+						Image={
 							'S3Object': {
 								'Bucket': 'noca-auth-library',
-								'Name': source_image_name
-							}
-						}, 
-						TargetImage={
-							'S3Object': {
-								'Bucket': 'noca-auth-library', 
 								'Name': image_name
 							}
-						}
+						},
+						Attributes=['DEFAULT']
 					)
-					print('rek response: ', response)
-					try:
-						similarity = response['FaceMatches'][0]['Similarity']
-						print('found faces')
-						if similarity > 85:
-							reply(sender, 'Your FaceID was authenticated successfully')
+					print('face detect: ', face_detect)
+					if len(face_detect['FaceDetails']) > 0:
+						if face_detect['FaceDetails'][0]['Confidence'] > 50:
+							source_image_name = '{name}/source/{name}-source.jpeg'.format(name=user.name)
+							print('source name: ', source_image_name)
+							print('target name: ', image_name)
+							
+							response = rek.compare_faces(
+								SimilarityThreshold=0,
+								SourceImage={
+									'S3Object': {
+										'Bucket': 'noca-auth-library',
+										'Name': source_image_name
+									}
+								}, 
+								TargetImage={
+									'S3Object': {
+										'Bucket': 'noca-auth-library', 
+										'Name': image_name
+									}
+								}
+							)
+							print('rek response: ', response)
+							try:
+								similarity = response['FaceMatches'][0]['Similarity']
+								print('found faces')
+								if similarity > 85:
+									reply(sender, 'Your FaceID was authenticated successfully')
+								else:
+									reply(sender, 'We didn\'t manage to authenticate your FaceID, please try again')
+
+							except IndexError:
+								print('Did not find a face in the image')
+								reply(sender, 'We didn\'t manage to authenticate your FaceID, please try again')
 						else:
-							reply(sender, 'We didn\'t manage to authenticate your FaceID, please try again')
-
-					except IndexError:
-						print('Did not find a face in the image')
-						reply(sender, 'We didn\'t manage to authenticate your FaceID, please try again')
-
-					
+							reply(sender, 'We didn\'t manage to find a face in the image')	
+					else:
+						reply(sender, 'We didn\'t manage to find a face in the image')
 
 					return 'ok'
 		elif 'postback' in data['entry'][0]['messaging'][0].keys():
